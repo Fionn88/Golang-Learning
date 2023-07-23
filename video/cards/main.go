fabdc092ea626d8ffe5438055000512db5f35041
FionnKuo <>
2023-07-23 21:56:18 +0800

    feat: refactor deck struct and main.go file in video/cards
    - Add `saveToFile` method to the `deck` struct in `video/cards/deck.go`
    - Add `newDeckFromFile` method to the `deck` struct in `video/cards/deck.go`
    - Add `shuffle` method to the `deck` struct in `video/cards/deck.go`
    - Comment out the code that prints the `cards` in `video/cards/main.go`
    - Comment out the code that saves the `cards` to a file in `video/cards/main.go`
    - Comment out the code that reads the `cards` from a file and prints it in `video/cards/main.go`
    - Uncomment the code that shuffles the `cards` and print it in `video/cards/main.go`
    - Add a new file `video/cards/my_cards`
    
    Signed-off-by: FionnKuo <>

--- video/cards/main.go
+++ video/cards/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
+	// ======== Printing Cards ========
 	// cards := newDeck()
-
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
+
+	// ======== Save File ========
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	// ======== Read File And Print It ========
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	// ======== shuffle Cards And Print It ========
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.shuffle()
+	cards.print()
 }
