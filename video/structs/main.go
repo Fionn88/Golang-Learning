dd06921f4bf38ff7c649dc78082cfb724e55152b
FionnKuo <>
2023-07-30 00:15:20 +0800

    adding jim person struct in struct

--- video/structs/main.go
+++ video/structs/main.go
@@ -5,17 +5,38 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
+	// ========== Alex ==========
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex)
 
-	var alex person
+	// ========== Alex Pointer ==========
+	// var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v\n", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v\n", alex)
+
+	// ========== Jim Person ==========
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	fmt.Printf("%+v\n", jim)
+
 }
