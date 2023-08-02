b01cb46d7d0ea6a5be8fe910e51c0addaa6797da
FionnKuo <>
2023-08-02 23:07:20 +0800

    The label that best describes this change is "refactor".: refactor color map declaration and manipulation
    - Declare an empty map called "colors"
    - Change the declaration of "colors" to use the "make" function instead of the literal syntax
    - Add a new key-value pair to the "colors" map
    - Delete a key from the "colors" map
    
    Signed-off-by: FionnKuo <>

--- video/map/main.go
+++ video/map/main.go
@@ -5,10 +5,21 @@ import (
 )
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// ======= Declare map =======
+	// var colors map[string]
+
+	// ======= Same Way To Declare =======
+	colors := make(map[int]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors[10] = "#ffffff"
+
+	// ======= Delete Map By Key =======
+	delete(colors, 10)
 
 	fmt.Println(colors)
 
