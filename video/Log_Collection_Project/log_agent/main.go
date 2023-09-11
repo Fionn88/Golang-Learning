90d7d53822cb67f2ca2966ee91a65b67b00ebd2b
Fionn88 <coco8802231225@gmail.com>
2023-09-12 00:45:36 +0800

    feat: add log collection project files
    - Add new file `video/Log_Collection_Project/log_agent/REAMDE.md`
    - Add new file `video/Log_Collection_Project/log_agent/main.go`
    
    Signed-off-by: Fionn88 <coco8802231225@gmail.com>

---
+++ video/Log_Collection_Project/log_agent/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+
+	client, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+	if err != nil {
+		fmt.Println("producer close, err:", err)
+		return
+	}
+	defer client.Close()
+
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "shoppings"
+	msg.Value = sarama.StringEncoder("2023.9.11Go")
+
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send message failed,", err)
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
